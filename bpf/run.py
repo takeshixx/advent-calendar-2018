77a6be3473d3cce0fee819223e91e2691a002015
Birk Kauer <bkauer@ernw.de>
2018-11-25 16:04:19 +0100

    BPF Challenge now Working... tested on ubuntu

--- bpf/run.py
+++ bpf/run.py
@@ -7,6 +7,13 @@ from sys import argv
 import sys
 import socket
 import os
+from scapy.all import Ether, IP, UDP, Raw
+import logging
+import asyncio
+import subprocess
+#logging.basicConfig(filename='bpf.log',level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
+ 
 
 #args
 def usage():
@@ -46,6 +53,11 @@ if len(argv) == 3:
 if len(argv) > 3:
   usage()
 
+success_text = open("./success", "rb").read()
+
+# HackyHacky wacky jacky :D
+p = subprocess.Popen("ncat -lk -p 8080 --sh-exec \"echo -e 'HTTP/1.1 200 OK\r\n'; cat index.html\"", shell=True)
+
 print ("binding socket to '%s'" % interface)
 
 # initialize BPF - load source code from parse.c
@@ -54,10 +66,12 @@ bpf = BPF(src_file = "parse.c",debug = 0)
 #load eBPF program filter of type SOCKET_FILTER into the kernel eBPF vm
 #more info about eBPF program types
 #http://man7.org/linux/man-pages/man2/bpf.2.html
+logging.info('JIT eBPF Code')
 function_filter = bpf.load_func("filter", BPF.SOCKET_FILTER)
 
 #create raw socket, bind it to interface
 #attach bpf program to socket created
+logging.info('Creating Raw Socket')
 BPF.attach_raw_socket(function_filter, interface)
 
 #get file descriptor of the socket previously created inside BPF.attach_raw_socket
@@ -68,71 +82,21 @@ sock = socket.fromfd(socket_fd,socket.PF_PACKET,socket.SOCK_RAW,socket.IPPROTO_I
 #set it as blocking socket
 sock.setblocking(True)
 
-while 1:
+while True:
   #retrieve raw packet from socket
   packet_str = os.read(socket_fd,2048)
-
-  #DEBUG - print raw packet in hex format
-  #packet_hex = toHex(packet_str)
-  #print ("%s" % packet_hex)
-
-  #convert packet into bytearray
   packet_bytearray = bytearray(packet_str)
 
-  #ethernet header length
-  ETH_HLEN = 14
-
-  #IP HEADER
-  #https://tools.ietf.org/html/rfc791
-  # 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-  # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-  # |Version|  IHL  |Type of Service|          Total Length         |
-  # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-  #
-  #IHL : Internet Header Length is the length of the internet header
-  #value to multiply * 4 byte
-  #e.g. IHL = 5 ; IP Header Length = 5 * 4 byte = 20 byte
-  #
-  #Total length: This 16-bit field defines the entire packet size,
-  #including header and data, in bytes.
-
-  #calculate packet total length
-  total_length = packet_bytearray[ETH_HLEN + 2]               #load MSB
-  total_length = total_length << 8                            #shift MSB
-  total_length = total_length + packet_bytearray[ETH_HLEN+3]  #add LSB
-
-  #calculate ip header length
-  ip_header_length = packet_bytearray[ETH_HLEN]               #load Byte
-  ip_header_length = ip_header_length & 0x0F                  #mask bits 0..3
-  ip_header_length = ip_header_length << 2                    #shift to obtain length
-
-  #TCP HEADER
-  #https://www.rfc-editor.org/rfc/rfc793.txt
-  #  12              13              14              15
-  #  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-  # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-  # |  Data |           |U|A|P|R|S|F|                               |
-  # | Offset| Reserved  |R|C|S|S|Y|I|            Window             |
-  # |       |           |G|K|H|T|N|N|                               |
-  # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-  #
-  #Data Offset: This indicates where the data begins.
-  #The TCP header is an integral number of 32 bits long.
-  #value to multiply * 4 byte
-  #e.g. DataOffset = 5 ; TCP Header Length = 5 * 4 byte = 20 byte
-
-  #calculate tcp header length
-  tcp_header_length = packet_bytearray[ETH_HLEN + ip_header_length + 12]  #load Byte
-  tcp_header_length = tcp_header_length & 0xF0                            #mask bit 4..7
-  tcp_header_length = tcp_header_length >> 2                              #SHR 4 ; SHL 2 -> SHR 2
-
-  #calculate payload offset
-  payload_offset = ETH_HLEN + ip_header_length + tcp_header_length
-
-  #Print
-  for i in range (payload_offset-1,len(packet_bytearray)-1):
-    if (packet_bytearray[i]== 0x0A):
-      if (packet_bytearray[i-1] == 0x0D):
-        break
-    print ("%c" % chr(packet_bytearray[i]), end = "")
-  print("")
+  # Parse RAW data using scapy.. super general and easy
+  c = Ether(packet_bytearray)
+  if c.haslayer(IP):
+    layer_ip = c.getlayer(IP)
+    src_address = layer_ip.src
+    logging.info('IP: {} said the magic word ;-)'.format(src_address))
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((src_address, 1337))
+      logging.info('Sending success text now to: {}'.format(src_address))
+      s.send(success_text)
+    except socket.error:
+      logging.info("Upps... no connection possible..")
