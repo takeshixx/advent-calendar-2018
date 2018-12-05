428a7bd14a0aa2b3ecb841a33301e4c676250dda
Birk Kauer <bkauer@ernw.de>
2018-12-05 18:52:21 +0100

    Cleanup of serial chal. Added Dockerfile and Readme

---
+++ serial/README.md
@@ -0,0 +1,14 @@
+# eBPF Service
+
+A simple Serial emulating Service. You will get challenges on one port and have to submit to the other Port. This can only be solved programmatically.
+
+## Building & Running
+
+```bash
+docker build -t day06_serial .
+docker run -d --restart=always -p 6:6 -p 66:66 --name=day06 day06_serial
+```
+
+## Solution
+
+No Solution yet.
--- serial/serial.go
+++ serial/serial.go
@@ -1,122 +1,118 @@
 package main
 
 import (
-	"net"
-	"time"
-	"strconv"
 	"fmt"
-	"strings"
+	"io/ioutil"
 	"log"
-	"reflect"
 	"math/rand"
+	"net"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
 )
-
 
 func writeConnection(conn net.Conn, challs []chalStruct, messages chan string) {
 	for len(messages) > 0 {
-	  <-messages
+		<-messages
 	}
 	var correct int
-        //fmt.Println("Write")
-	//fmt.Printf("%+v\n", challs)
-	if len(challs) == 0{
+	if len(challs) == 0 {
 		conn.Write([]byte("Something went wrong on our side :( connect again"))
 		conn.Close()
 		return
 	}
-        for {
+	for {
 		for _, chal := range challs {
-			messages <-string(chal.challenge + "\n")
-			//conn.Write([]byte(chal.challenge))
-			//fmt.Print(chal.result)
+			messages <- string(chal.challenge + "\n")
 			buffer := make([]byte, 1400)
-	                dataSize, err := conn.Read(buffer)
-	                if err != nil {
-	                    log.Println("Connection has closed")
-	                    return
-	                }
+			dataSize, err := conn.Read(buffer)
+			if err != nil {
+				log.Println("Connection has closed")
+				return
+			}
 
-			 //This is the message you received
-		        data := buffer[:dataSize-1]
+			//This is the message you received
+			data := buffer[:dataSize-1]
 			int_input, _ := strconv.Atoi(string(data))
 			log.Printf("Client Answered: %d", int_input)
-			if int_input == chal.result{
+			if int_input == chal.result {
 				correct++
-				if correct >= len(challs){
-					messages <- "YOU SOLVED IT HAPPY XMAS YAY\n"
+				if correct >= len(challs) {
+					messages <- "SOLVED\n"
 					conn.Close()
 				}
-				messages <-"Congratulations... This was correct \n"
+				messages <- "Congratulations... This was correct \n"
 			} else {
-				messages <-"Wrong\n"
+				messages <- "Wrong\n"
 				log.Printf("Client got challenge wrong.. Lets kick him")
 				conn.Close()
 				return
 			}
-	                //fmt.Print("Received message: ", string(int_input))
-			//messages <-string(data)
-		}
-                // Send Victory Message
-        }
-}
-
-func readConnection(conn net.Conn, messages chan string) {
-	afterCh := time.After(20 * time.Second)
-	for {
-	    select {
-	    case d := <-messages:
-		    if strings.Contains(d, "Wrong") {
-			conn.Write([]byte(d))
-			conn.Close()
-		    }
-		    if strings.Contains(d, "SOLVED"){
-			conn.Write([]byte(d))
-			conn.Close()
-		    }
-		conn.Write([]byte(d))
-	        log.Println("Channel Sending:", d)
-		break
-	    case <-afterCh:
-	        log.Printf("Time's up for %s", conn.RemoteAddr())
-		conn.Write([]byte("Your Time is up. Tschingle Fail\n"))
-		conn.Close()
-	        break
 		}
 	}
 }
 
+func readConnection(conn net.Conn, messages chan string) {
+	afterCh := time.After(10 * time.Second)
+	for {
+		select {
+		case d := <-messages:
+			if strings.Contains(d, "Wrong") {
+				conn.Write([]byte(d))
+				conn.Close()
+			}
+			if strings.Contains(d, "SOLVED") {
+				success, err := ioutil.ReadFile("./success")
+				if err != nil {
+					log.Fatal("Success File not there ?")
+				}
+				conn.Write(success)
+				conn.Close()
+			}
+			conn.Write([]byte(d))
+			log.Println("Channel Sending:", d)
+			break
+		case <-afterCh:
+			log.Printf("Time's up for %s", conn.RemoteAddr())
+			conn.Write([]byte("Your Time is up. Tschingle Fail\n"))
+			conn.Close()
+			break
+		}
+	}
+}
 
 func main() {
-    var challs map[string][]chalStruct
-    challs = make(map[string][]chalStruct)
+	var challs map[string][]chalStruct
+	challs = make(map[string][]chalStruct)
 
-    messages := make(map[string](chan string))
-    fmt.Println("The server is listening on Port 8080")
-    listener, err := net.Listen("tcp", "localhost:8080")
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("The server2 is listening on Port 8081")
-    listener2, err := net.Listen("tcp", "localhost:8081")
-    if err != nil {
-        log.Fatal(err)
-    }
-    go acceptLoop(listener, challs, messages)
-    acceptLoop(listener2, challs, messages)  // run in the main goroutine
+	messages := make(map[string](chan string))
+	fmt.Println("The server is listening on Port 6")
+	listener, err := net.Listen("tcp", "localhost:6")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("The server2 is listening on Port 66")
+	listener2, err := net.Listen("tcp", "localhost:66")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go acceptLoop(listener, challs, messages)
+	acceptLoop(listener2, challs, messages) // run in the main goroutine
 }
 
 func RandomInt(min int, max int) int {
-    r := rand.New(rand.NewSource(time.Now().UnixNano()))
-    p := r.Perm(max - min + 1)
-    return p[min]
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	p := r.Perm(max - min + 1)
+	return p[min]
 }
 
-func create_challenge()(string, int){
+func create_challenge() (string, int) {
 	var function_operators = map[string]interface{}{
-		"+": func (x int, y int)int {return x+y},
-		"-": func (x int, y int)int {return x-y},
-		"*": func (x int, y int)int {return x*y},
-		"/": func (x int, y int)int {return x/y},
+		"+": func(x int, y int) int { return x + y },
+		"-": func(x int, y int) int { return x - y },
+		"*": func(x int, y int) int { return x * y },
+		"/": func(x int, y int) int { return x / y },
 	}
 
 	keys := reflect.ValueOf(function_operators).MapKeys()
@@ -124,54 +120,51 @@ func create_challenge()(string, int){
 	n1 := RandomInt(0, 100000)
 	// Avoiding Division by Zero
 	var n2 int
-	if rand_operator == "/"{
+	if rand_operator == "/" {
 		n2 = RandomInt(1, 100000)
 	} else {
 		n2 = RandomInt(0, 100000)
 	}
-	res := function_operators[rand_operator].(func(int, int)int)(n1,n2)
+	res := function_operators[rand_operator].(func(int, int) int)(n1, n2)
 	return strconv.Itoa(n1) + rand_operator + strconv.Itoa(n2), res
 }
 
-
-type chalStruct struct{
+type chalStruct struct {
 	challenge string
-	result int
+	result    int
 }
 
 func acceptLoop(l net.Listener, challs map[string][]chalStruct, m map[string](chan string)) {
-    defer l.Close()
-    for {
-            c, err := l.Accept()
-            if err != nil {
-                log.Fatal(err)
-            }
-	    defer l.Close()
-	    var ipChallenges []chalStruct
-	    local_port := strings.Split(c.LocalAddr().String(), ":")
-	    ip := strings.Split(c.RemoteAddr().String(), ":")
-	    log.Printf("New connection found -> %s\n", ip[0])
-	    if local_port[1] == "8080"{
-		    amount := RandomInt(2,10)
-		    for i:=0; i<= amount; i++ {
-			    chal, result := create_challenge()
-			    test := chalStruct{challenge: chal, result: result}
-			    ipChallenges = append(ipChallenges, test)
-			    //fmt.Printf("Challenge :%s Result: %d\n", test.challenge, test.result)
-		   }
-		   log.Printf("Created new Challenges for IP: %s", ip[0])
-		   //fmt.Println(ipChallenges)
-		   m[ip[0]] = make(chan string) 
-		   challs[ip[0]] = ipChallenges
-		go readConnection(c, m[ip[0]])
-	    } else {
-		if val, ok := challs[ip[0]]; ok {
-			go writeConnection(c, val, m[ip[0]])
-		} else {
-			c.Write([]byte("It is SERIAL important that you do other Stuff first\n"))
-			log.Printf("Sending Go Away Message to %s\n", ip[0])
-			c.Close()
+	defer l.Close()
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
 		}
-	    }
-    }
+		defer l.Close()
+		var ipChallenges []chalStruct
+		local_port := strings.Split(c.LocalAddr().String(), ":")
+		ip := strings.Split(c.RemoteAddr().String(), ":")
+		log.Printf("New connection found -> %s\n", ip[0])
+		if local_port[1] == "6" {
+			amount := RandomInt(2, 10)
+			for i := 0; i <= amount; i++ {
+				chal, result := create_challenge()
+				test := chalStruct{challenge: chal, result: result}
+				ipChallenges = append(ipChallenges, test)
+			}
+			log.Printf("Created new Challenges for IP: %s", ip[0])
+			m[ip[0]] = make(chan string)
+			challs[ip[0]] = ipChallenges
+			go readConnection(c, m[ip[0]])
+		} else {
+			if val, ok := challs[ip[0]]; ok {
+				go writeConnection(c, val, m[ip[0]])
+			} else {
+				c.Write([]byte("It is SERIAL important that you do other Stuff first\n"))
+				log.Printf("Sending Go Away Message to %s\n", ip[0])
+				c.Close()
+			}
+		}
+	}
 }
