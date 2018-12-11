e4ee4aade10e07781cbd7b3c6a58e2843a2ecda0
takeshix <takeshix@adversec.com>
2018-12-11 20:05:22 +0100

    Added secret to tcpmux

--- tcpmux/wishlist.py
+++ tcpmux/wishlist.py
@@ -42,4 +42,5 @@ print('Santa received the following list:')
 for i in wishlist:
     print('\t' + i)
 print('Let\'s see what Santa can do for you, bye bye!')
+print('zYkmesG45dIDt4mSHs9OlpFESzq0dbDsrUZ0M1DcfYcdEdguOVdG0mc/KER3BO2W')
 sys.stdout.flush()
