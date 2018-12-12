72f247a72128819b10d6ef566761afd66df770d5
takeshix <takeshix@adversec.com>
2018-12-12 22:14:45 +0100

    Updated xmassshhttp

--- xmassshhttp/run.sh
+++ xmassshhttp/run.sh
@@ -2,4 +2,4 @@
 set -e
 /usr/sbin/nginx
 /usr/sbin/sshd
-/usr/src/app/tcpmux/tcpmux.py --listen 0.0.0.0:15 --ssh localhost:22 --tls loclhost:443 --other loclhost:443
+python3 /usr/src/app/tcpmux/tcpmux.py --listen 0.0.0.0:15 --ssh localhost:22 --tls loclhost:443 --other loclhost:443
