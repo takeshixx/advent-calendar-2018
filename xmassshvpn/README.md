26488120143f0a7a019ac0c8ce9c1ad0f7744c58
takeshix <takeshix@adversec.com>
2018-12-20 12:43:34 +0100

    Updated xmassshvpn

--- xmassshvpn/README.md
+++ xmassshvpn/README.md
@@ -6,5 +6,12 @@ The challenge is to send DHCP requests to the host via a SSH layer 2 VPN.
 
 ```bash
 docker build -t day23_xmassshvpn .
-docker run -d --restart=always -p 23:22 --cap-add=NET_ADMIN --cap-add=NET_RAW --name=day23 day23_xmassshvpn
+docker run -d --restart=always -p 23:2222 --privileged --cap-add=NET_ADMIN --cap-add=NET_RAW --name=day23 day23_xmassshvpn
+```
+
+## Testing
+
+```bash
+sudo ssh -v -o Tunnel=ethernet -w 1:1 -p 23 santa@xmas.rip
+sudo dhcpcd -T tap1
 ```
--- xmassshvpn/run.sh
+++ xmassshvpn/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 /sbin/ip link add name br0 type bridge
+/sbin/ip addr add 10.23.13.37/24 dev br0
 /sbin/ip link set br0 up
 /sbin/ip tuntap add dev tap0 mode tap
 /sbin/ip link set tap0 master br0
@@ -8,11 +9,8 @@ set -e
 /sbin/ip tuntap add dev tap1 mode tap
 /sbin/ip link set tap1 master br0
 /sbin/ip link set up dev tap1
-if [ ! -f /var/lib/dhcpd/dhcpd.leases ]; then
-    mkdir -p /var/lib/dhcpd/
-    touch /var/lib/dhcpd/dhcpd.leases
-fi
-/bin/chmod 755 /usr/lib
-#/usr/sbin/dhcpd -cf /etc/dhcpd.conf br0
-/usr/sbin/dhcpd -cf /etc/dhcpd.conf -f br0
-#/usr/sbin/sshd -D -p 2222
+/usr/sbin/dhcpd \
+        -cf /etc/dhcpd.conf \
+        -lf /usr/src/app/leases \
+        br0
+/usr/sbin/sshd -D -p 2222
